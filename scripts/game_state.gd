520e7d858144a09e5f45f37997ddc7f37adc95d5
erkek2000 <mobulutnetas@gmail.com>
2026-06-06 21:24:32 +0300

    Added pause

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,12 +1,23 @@
 extends Node
 
 signal inventory_toggled(is_open: bool)
+signal pause_toggled(is_paused: bool)
 
 var inventory_open: bool = false :
 	set(value):
 		inventory_open = value
-		Input.set_mouse_mode(
-			Input.MOUSE_MODE_VISIBLE if value
-			else Input.MOUSE_MODE_CAPTURED
-		)
 		inventory_toggled.emit(value)
+		_update_mouse_mode()
+
+var paused: bool = false :
+	set(value):
+		paused = value
+		get_tree().paused = value
+		pause_toggled.emit(value)
+		_update_mouse_mode()
+
+func _update_mouse_mode() -> void:
+	Input.set_mouse_mode(
+		Input.MOUSE_MODE_VISIBLE if (inventory_open or paused)
+		else Input.MOUSE_MODE_CAPTURED
+	)
--- scripts/interface.gd
+++ scripts/interface.gd
@@ -2,11 +2,26 @@ extends Control
 
 
 @onready var inventory_panel: Control = $InventoryPanel
+@onready var pause_menu = $PauseMenu
+@onready var resume_button = $PauseMenu/VBoxContainer/ResumeButton
+@onready var quit_button = $PauseMenu/VBoxContainer/QuitButton
+
 
 func _ready() -> void:
-	inventory_panel.visible = false
+	GameState.inventory_toggled.connect(_on_inventory_toggled)
+	GameState.pause_toggled.connect(_on_pause_toggled)
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory"):
 		GameState.inventory_open = not GameState.inventory_open
-		inventory_panel.visible = GameState.inventory_open
+	elif event.is_action_pressed("pause_menu"):
+		GameState.paused = not GameState.paused
+
+
+func _on_inventory_toggled(is_open: bool) -> void:
+	inventory_panel.visible = is_open
+
+
+func _on_pause_toggled(is_paused: bool) -> void:
+	pause_menu.visible = is_paused
