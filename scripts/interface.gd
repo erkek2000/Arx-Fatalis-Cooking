d1493d18929cfa4ef6ba80ac2158ba6e8e44c612
erkek2000 <mobulutnetas@gmail.com>
2026-06-06 21:28:14 +0300

    pausemenu functional

--- scripts/interface.gd
+++ scripts/interface.gd
@@ -10,14 +10,14 @@ extends Control
 func _ready() -> void:
 	GameState.inventory_toggled.connect(_on_inventory_toggled)
 	GameState.pause_toggled.connect(_on_pause_toggled)
-
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory"):
 		GameState.inventory_open = not GameState.inventory_open
 	elif event.is_action_pressed("pause_menu"):
 		GameState.paused = not GameState.paused
-
+	
 
 func _on_inventory_toggled(is_open: bool) -> void:
 	inventory_panel.visible = is_open
@@ -25,3 +25,10 @@ func _on_inventory_toggled(is_open: bool) -> void:
 
 func _on_pause_toggled(is_paused: bool) -> void:
 	pause_menu.visible = is_paused
+
+
+func _on_resume_button_pressed() -> void:
+	GameState.paused = false
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
